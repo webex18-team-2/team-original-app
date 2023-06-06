88ee8f3493af1b67e048c44868a33aac09580afa
YUEJIAWEN <rkeibun@gmail.com>
2023-06-06 13:52:06 +0900

    vue-routerを導入

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
+import AboutView from "../views/AboutView.vue"
+import ReadView from "../views/ReadView.vue"
 
 const routes = [
   {
@@ -13,8 +15,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AboutView,
+  },
+  {
+    path: "/read-page",
+    name: "ReadPage",
+    component: ReadView,
   },
 ]
 
