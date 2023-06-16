cd4c6d719d37e01078b9e9632f8b0b1689eb4785
YUEJIAWEN <rkeibun@gmail.com>
2023-06-16 19:02:29 +0900

    homepage-1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-    <router-link to="/">HomePage</router-link> |
-    <router-link to="/write">入力画面</router-link> |
-    <router-link to="/read-page">閲覧画面</router-link>|
-    <router-link to="/calendar">カレンダー</router-link>|
-    <router-link to="/analysis">分析</router-link>
+  <div class="home">
+    <div>
+      <router-link to="/">HomePage|</router-link>
+
+      <router-link to="/write">入力画面|</router-link>
+
+      <router-link to="/read-page">閲覧画面|</router-link>
+
+      <router-link to="/calendar">カレンダー|</router-link>
+
+      <router-link to="/analysis">分析</router-link>
+    </div>
   </div>
   <router-view />
 </template>
