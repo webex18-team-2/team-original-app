e5f2888e937031b12b671176557bed339c9a400c
RinzGit <200fe632@st.nufs.ac.jp>
2023-06-18 18:27:26 +0900

    グラフの原型を作成しました

--- src/views/AnalysisView.vue
+++ src/views/AnalysisView.vue
@@ -1,5 +1,64 @@
 <template>
   <div class="analysis">
     <h1>分析</h1>
+    <Line :data="data" :options="options" />
   </div>
 </template>
+
+<script>
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js"
+import { Line } from "vue-chartjs"
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+export default {
+  name: "App",
+  components: {
+    Line,
+  },
+  data() {
+    return {
+      data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [40, 39, 10, 40, 39, 80, 40],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    }
+  },
+}
+</script>
+
+<style></style>
